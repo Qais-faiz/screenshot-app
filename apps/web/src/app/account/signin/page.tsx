169aa8f84c6057b0839529ba03614598a81b0118
Qais-faiz <qaisfaiz80@gmail.com>
2025-12-01 11:23:08 +0430

    Fix: Remove duplicate pages from src/app, keep only in app directory

--- apps/web/src/app/account/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-import SignInClient from './signin-client';
-
-export const dynamic = 'force-dynamic';
-
-export default function SignInPage() {
-  return <SignInClient />;
-}
--- apps/web/src/app/account/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-import SignUpClient from './signup-client';
-
-export const dynamic = 'force-dynamic';
-
-export default function SignUpPage() {
-  return <SignUpClient />;
-}
